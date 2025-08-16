4be89499bea14905a5ebaab4a6b40d7d71f7024a
masatoomori <23582786+masatoomori@users.noreply.github.com>
2025-08-16 21:27:30 +0900

    Update website.tf

--- infra/terraform/website.tf
+++ infra/terraform/website.tf
@@ -57,3 +57,44 @@ resource "google_storage_bucket_object" "not_found_html" {
   # Generate MD5 hash to detect changes
   detect_md5hash = filemd5("${path.root}/../../website/404.html")
 }
+
+# Serve www subdomain via HTTPS Load Balancer with Managed SSL
+# Backend bucket using the same storage bucket
+resource "google_compute_backend_bucket" "website_backend" {
+  name        = "${local.env}-website-backend"
+  bucket_name = google_storage_bucket.website.name
+}
+
+# URL map to direct requests to the backend bucket
+resource "google_compute_url_map" "website_map" {
+  name            = "${local.env}-website-map"
+  default_service = google_compute_backend_bucket.website_backend.id
+}
+
+# Managed SSL certificate for www domain
+resource "google_compute_managed_ssl_certificate" "www_cert" {
+  name = "${local.env}-www-cert"
+  managed {
+    domains = [local.www_domain]
+  }
+}
+
+# Target HTTPS proxy using the managed cert and URL map
+resource "google_compute_target_https_proxy" "https_proxy" {
+  name             = "${local.env}-https-proxy"
+  url_map          = google_compute_url_map.website_map.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.www_cert.id]
+}
+
+# Global forwarding rule to accept HTTPS traffic
+resource "google_compute_global_address" "lb_ip" {
+  name = "${local.env}-lb-ip"
+}
+
+resource "google_compute_global_forwarding_rule" "https_forwarding" {
+  name        = "${local.env}-https-forwarding"
+  ip_address  = google_compute_global_address.lb_ip.address
+  ip_protocol = "TCP"
+  port_range  = "443"
+  target      = google_compute_target_https_proxy.https_proxy.id
+}
